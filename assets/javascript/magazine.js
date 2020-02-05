f76378970a10e0b7b09407742d076b1e97a01df7
Jason Schutz <jason@rogueathletic.com>
2020-02-05 08:11:55 -0800

    temp set images to absolute path and added meta og tags for test review

--- assets/javascript/magazine.js
+++ assets/javascript/magazine.js
@@ -46,7 +46,7 @@ function loadPage(page, pageElement) {
 
 	// Load the page
 
-	img.attr('src', '/caldera-flipw-slider-zoom/assets/pages/' +  page + '.jpg');
+	img.attr('src', 'https://rogueathletic.github.io/caldera-flipw-slider-zoom/assets/pages/' +  page + '.jpg');
 
 	loadRegions(page, pageElement);
 
@@ -76,7 +76,7 @@ function zoomTo(event) {
 
 function loadRegions(page, element) {
 
-	$.getJSON('/caldera-flipw-slider-zoom/assets/pages/'+page+'-regions.json').
+	$.getJSON('https://rogueathletic.github.io/caldera-flipw-slider-zoom/assets/pages/'+page+'-regions.json').
 		done(function(data) {
 
 			$.each(data, function(key, region) {
@@ -177,7 +177,7 @@ function loadLargePage(page, pageElement) {
 
 	// Loadnew page
 	
-	img.attr('src', '/caldera-flipw-slider-zoom/assets/pages/' +  page + '-large.jpg');
+	img.attr('src', 'https://rogueathletic.github.io/caldera-flipw-slider-zoom/assets/pages/' +  page + '-large.jpg');
 }
 
 // Load small page
@@ -191,7 +191,7 @@ function loadSmallPage(page, pageElement) {
 	img.unbind('load');
 	// Loadnew page
 
-	img.attr('src', '/caldera-flipw-slider-zoom/assets/pages/' +  page + '.jpg');
+	img.attr('src', 'https://rogueathletic.github.io/caldera-flipw-slider-zoom/assets/pages/' +  page + '.jpg');
 }
 
 // http://code.google.com/p/chromium/issues/detail?id=128488
@@ -300,7 +300,7 @@ function setPreview(view) {
 
 	var previewWidth = 112,
 		previewHeight = 73,
-		previewSrc = '/caldera-flipw-slider-zoom/assets/pages/preview.jpg',
+		previewSrc = 'https://rogueathletic.github.io/caldera-flipw-slider-zoom/assets/pages/preview.jpg',
 		preview = $(_thumbPreview.children(':first')),
 		numPages = (view==1 || view==$('#slider').slider('option', 'max')) ? 1 : 2,
 		width = (numPages==1) ? previewWidth/2 : previewWidth;
